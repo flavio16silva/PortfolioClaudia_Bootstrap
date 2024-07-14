d27a6f52f99f242ebf44c67a2cb1b591e8aeffb0
Flavio Silva <fal_silva@hotmail.com>
2024-07-14 19:47:11 -0300

    arquivos js na pasta raiz

---
+++ scripts/escrita.js
@@ -0,0 +1,17 @@
+// var typed = new Typed(".escrever-texto", {
+//   strings: ["avalio para porte de armas", "realizo seleção e treinamento", "faço avaliação de desempenho."],
+//   loop: true,
+//   typeSpeed: 50,
+//   backSpeed: 25,
+//   backDelay: 500
+// });
+
+document.addEventListener("DOMContentLoaded", function() {
+  var typed = new Typed(".escrever-texto", {
+    strings: ["avaliação para manuseio de armas de fogo.", "seleção e treinamento.", "avaliação de desempenho."],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500
+  });
+});

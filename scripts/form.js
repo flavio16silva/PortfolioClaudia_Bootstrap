8325d0cbb898cae8919c6bf24dd31d78d2fbb791
Flavio Silva <fal_silva@hotmail.com>
2024-07-18 21:47:29 -0300

    form consultoria

---
+++ scripts/form.js
@@ -0,0 +1,33 @@
+  // document.querySelectorAll('.form-control').forEach(input => {
+  //   input.addEventListener('focus', () => {
+  //     const label = input.nextElementSibling;
+  //     if (label && label.classList.contains('form-label')) {
+  //       label.style.display = 'none';
+  //     }
+  //   });
+  //   input.addEventListener('blur', () => {
+  //     if (!input.value) {
+  //       const label = input.nextElementSibling;
+  //       if (label && label.classList.contains('form-label')) {
+  //         label.style.display = 'block';
+  //       }
+  //     }
+  //   });
+  // });
+
+  // document.querySelectorAll('.form-control').forEach(input => {
+  //   input.addEventListener('focus', () => {
+  //     const label = input.nextElementSibling;
+  //     if (label && label.classList.contains('form-label')) {
+  //       label.style.opacity = '0';
+  //     }
+  //   });
+  //   input.addEventListener('blur', () => {
+  //     if (!input.value) {
+  //       const label = input.nextElementSibling;
+  //       if (label && label.classList.contains('form-label')) {
+  //         label.style.opacity = '1';
+  //       }
+  //     }
+  //   });
+  // });
